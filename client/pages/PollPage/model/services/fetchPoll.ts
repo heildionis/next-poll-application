d3ba3d97602f27b2fa946f96da2d77408c07a79f
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-07-03 00:01:26 +0300

    Fix: build

--- client/pages/PollPage/model/services/fetchPoll.ts
+++ client/pages/PollPage/model/services/fetchPoll.ts
@@ -4,26 +4,21 @@ import { fetchPollFromApi } from '../../api/pollsApi';
 
 import { ThunkConfig } from '#/app/providers/StoreProvider';
 import { Poll } from '#/entities/Poll';
-import { hasFetchBaseQueryError } from '#/shared/lib/store/query/types/hasError';
 
-export const fetchPoll = createAsyncThunk<Poll, string, ThunkConfig<string>>(
-    'pollPage/fetchPoll',
-    async (shareableUrl, thunkApi) => {
-        const { dispatch, rejectWithValue } = thunkApi;
+export const fetchPoll = createAsyncThunk<
+    { poll: Poll; isVoteExist: boolean },
+    string,
+    ThunkConfig<string>
+>('pollPage/fetchPoll', async (shareableUrl, thunkApi) => {
+    const { dispatch, rejectWithValue } = thunkApi;
 
-        try {
-            const response = await dispatch(fetchPollFromApi(shareableUrl));
+    try {
+        const response = await dispatch(fetchPollFromApi(shareableUrl));
 
-            if (hasFetchBaseQueryError(response)) {
-                const { error } = response;
-                return rejectWithValue(error.data as string);
-            }
-
-            if ('data' in response) {
-                return response.data;
-            }
-        } catch (error) {
-            return rejectWithValue(error);
+        if ('data' in response) {
+            return response.data;
         }
+    } catch (error) {
+        return rejectWithValue(error);
     }
-);
+});
